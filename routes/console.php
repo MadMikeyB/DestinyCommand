08d92ef7885f8165b1d6ee58bb67599d7fe46697
Mikey Burton <mikeybsaccount@gmail.com>
2026-07-17 13:43:29 +0100

    Remove Inspire

--- routes/console.php
+++ routes/console.php
@@ -1,12 +1,6 @@
 <?php
 
 use App\Console\Commands\UpdateDestinyManifest;
-use Illuminate\Foundation\Inspiring;
-use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
-
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote');
 
 Schedule::command(UpdateDestinyManifest::class)->daily();
